acb2df2de7cd2040e8244a80bb11696f1e37e317
erik <kire481@gmail.com>
2022-11-10 19:27:54 +0100

    cosas

---
+++ routes/discoRouter.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const Disco = require("../models/disco")
+const discoRouter = express.Router();
+
+discoRouter.post("/disco", async (req,res)=>{
+    const {title, description} = req.body;
+    try {
+        if(!title || !description || !author){
+            return res.status(400).json({
+                success: false,
+                message: "Please fill all the fields"
+            })
+    
+        }
+        if(title.length < 3 || description.length <10 || author.length <3){
+            return res.status(400).json({
+                success: false,
+                message: "Title must be between 3 and 50 characters"
+            })
+        }
+        if(description.length < 3){
+            return res.status(400).json({
+                success: false,
+                message: "Description must be between 10 and 450 characters"
+            })
+        }
+        if(author.length < 3){
+            return res.status(400).json({
+                success: false,
+                message: "Author mus be between 3 and 50 characters"
+            })
+        }
+        let disco = new Disco ({
+           title,
+           description,
+           author
+        })
+        await disco.save()
+        return res.status(200).json({
+            success: true,
+            disco,
+            message: "Disco created successfully"
+        })
+    }catch (error){
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+    
+}  )
+
+
+module.exports = discoRouter;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+
+
 require("dotenv").config();
 const mongoose = require("mongoose");
 
 const URL = process.env.MONGODB_URL;
+
+const discoRouter = require("./routes/discoRouter")
+
+app.use(express.json({extended: true}));
+app.use(express.urlencoded());
 
 mongoose
   .connect(URL, {})
@@ -15,6 +22,8 @@ mongoose
   });
 
 // console.log("Hola")
+
+app.use("/api", discoRouter)
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
