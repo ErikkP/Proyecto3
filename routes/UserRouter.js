37196d84be45ae231d2d6b2f0258a74ad3ce920e
erik <kire481@gmail.com>
2022-11-16 17:33:22 +0100

    Token

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const User = require("../models/User");
 const UserRouter = express.Router();
+const bcrypt = require("bcrypt");
+// const { JsonWebTokenError } = require("jsonwebtoken");
+const salt = bcrypt.genSaltSync(10);
+const jwt = require('jsonwebtoken');
+
+const createToken = (user) => {
+    return jwt.sign(user, proccess.env.ACCES_TOKEN_SECRET, {expiresIn: '7d'});
+};
 
 
-UserRouter.post("/user", async(req,res)=>{
+
+
+
+UserRouter.post("/register", async(req,res)=>{
     const {name, email, password} = req.body
     try{
         const user = await User.findOne({email})
@@ -46,24 +57,37 @@ UserRouter.post("/user", async(req,res)=>{
 
         const validatePassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/
 
+       
 
         if(!validateEmail.test(email)){
             return res.status(400).json({
                 success: false,
                 message: "El correo no és valido"
-            })
+            });
         }
+
+        if(!validatePassword.test(password)){
+            return res.status(400).json({
+                success: false,
+                message: "La contraseña debe tener al menos una mayúscula, una minúscula y un número",
+            });
+        }
+
+
+        let passwordHash = bcrypt.hashSync(password, salt);
         let newUser = new User ({
             name,
             email,
-            password
-        })
+            password:passwordHash,
+        });
 
         await newUser.save()
+        const accessToken = createToken({ id: newUser._id })
         return res.status(200).json({
             success: true,
             newUser,
-            message: "Usuario creado con éxito"
+            message: "Usuario creado con éxito",
+            accessToken,
         })
     } catch(error) {
         return res.status(500).json({
@@ -72,6 +96,37 @@ UserRouter.post("/user", async(req,res)=>{
         })
 
     }
-})
+});
+
+
+UserRouter.post("/login", async(req,res)=>{
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "Alguno de los datos son incorrectos (correo)"
+            })
+        }
+        const passwordOK = bcrypt.compareSync(password, user.password);
+        if(passwordOK){
+            return res.status(400).json({
+                success: false,
+                message: "Alguno de los datos son incorrectos (password)"
+            })
+        }
+
+        return res.statusMessage(200).json({
+            success: true,
+            message:"Usuario logueado con éxito"
+        })
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+});
 
 module.exports = UserRouter;
